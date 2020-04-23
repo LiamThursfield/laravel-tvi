dc8e0ba95bc6fcd929d29443fc5e686c12a2fc5c
Liam Thursfield <liam3006x@gmail.com>
2020-04-24 00:15:55 +0100

    Replace bootstrap with tailwind

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,15 +1,19 @@
 const mix = require('laravel-mix');
+const tailwindcss = require('tailwindcss');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
 
-mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+/** App **/
+mix
+    .js('resources/js/app.js', 'public/js')
+    .version();
+mix
+    .postCss('resources/css/app.css', 'public/css')
+    .options({
+        postCss: [
+            require('postcss-import'),
+            require('postcss-nested'),
+            tailwindcss('./tailwind.config.js'),
+            require('autoprefixer')
+        ]
+    })
+    .version();
