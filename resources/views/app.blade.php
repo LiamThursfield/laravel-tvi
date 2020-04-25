382606f83d8a19dfc0841e8ebed0fa1ed111b91c
Liam Thursfield <liam3006x@gmail.com>
2020-04-25 15:40:24 +0100

    Install ziggy

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,10 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
+
+    @routes
     <script src="{{ mix('/js/app.js') }}" defer></script>
 </head>
 <body>
