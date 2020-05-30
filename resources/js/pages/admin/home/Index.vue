2b7f1663784f4c41c0821d008c47181c0e31ef4c
Liam Thursfield <liam3006x@gmail.com>
2020-05-30 15:48:17 +0100

    Basic admin index page

---
+++ resources/js/pages/admin/home/Index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <p>Admin</p>
+        <p>{{ $page.auth.user.name }}</p>
+        <inertia-link class="block px-6 py-2 hover:bg-indigo-500 hover:text-white" :href="$route('logout')" method="post">Logout</inertia-link>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        name: "AdminHomeIndex",
+        layout: 'admin-layout',
+    }
+</script>
