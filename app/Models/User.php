cf488d6c34a674d064f6aa271ecdabc6c23b8416
Liam Thursfield <liam3006x@gmail.com>
2020-05-30 14:57:33 +0100

    User - replace name with first and last name fields

--- app/Models/User.php
+++ app/Models/User.php
@@ -36,4 +36,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function getNameAttribute() {
+        return $this->first_name . ' ' . $this->last_name;
+    }
 }
