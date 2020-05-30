457f226463cd79a3acaf4903d792177e629b4e93
Liam Thursfield <liam.thursfield@nexuspoint.co.uk>
2020-05-30 14:28:30 +0100

    Remove redundant inports

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,11 +2,7 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Config;
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\Session;
-use Inertia\Inertia;
 
 class AppServiceProvider extends ServiceProvider
 {
