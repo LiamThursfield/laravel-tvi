c5aa791c1fb38c1c5ac001d73fa370bc437c3323
Liam Thursfield <liam3006x@gmail.com>
2020-05-30 15:48:39 +0100

    Implement register

---
+++ resources/js/pages/auth/Register.vue
@@ -0,0 +1,145 @@
+<template>
+    <div class="max-w-md w-full">
+        <div
+            class="bg-gray-200 rounded-md shadow-md"
+        >
+            <h1
+                class="font-bold pb-6 pt-8 px-6 text-center text-gray-700 text-2xl tracking-wide"
+            >
+                Welcome
+            </h1>
+
+
+            <form
+                class="px-6"
+                @submit.prevent="submit"
+            >
+                <div
+                    class="
+                        flex flex-col
+                        md:flex-row md:space-x-2
+                    "
+                >
+
+
+                <input-group
+                    class="mt-4"
+                    :error_message="$page.errors.first_name ? $page.errors.first_name[0] : ''"
+                    input_autocomplete="given-name"
+                    :input_autofocus="true"
+                    input_id="first_name"
+                    input_name="first_name"
+                    :input_required="true"
+                    input_type="first_name"
+                    label_text="First Name"
+                    v-model="form.first_name"
+                />
+
+                <input-group
+                    class="mt-4"
+                    :error_message="$page.errors.last_name ? $page.errors.last_name[0] : ''"
+                    input_autocomplete="family-name"
+                    input_id="last_name"
+                    input_name="last_name"
+                    :input_required="true"
+                    input_type="last_name"
+                    label_text="Last Name"
+                    v-model="form.last_name"
+                />
+
+                </div>
+
+                <input-group
+                    class="mt-4"
+                    :error_message="$page.errors.email ? $page.errors.email[0] : ''"
+                    input_autocomplete="email"
+                    input_id="email"
+                    input_name="email"
+                    :input_required="true"
+                    input_type="email"
+                    label_text="Email"
+                    v-model="form.email"
+                />
+
+                <input-group
+                    class="mt-4"
+                    :error_message="$page.errors.password ? $page.errors.password[0] : ''"
+                    input_autocomplete="new-password"
+                    input_id="password"
+                    input_name="password"
+                    :input_required="true"
+                    input_type="password"
+                    label_text="Password"
+                    v-model="form.password"
+                />
+
+                <input-group
+                    class="mt-4"
+                    :error_message="$page.errors.password_confirmation ? $page.errors.password_confirmation[0] : ''"
+                    input_autocomplete="new-password"
+                    input_id="password_confirmation"
+                    input_name="password_confirmation"
+                    :input_required="true"
+                    input_type="password"
+                    label_text="Confirm Password"
+                    v-model="form.password_confirmation"
+                />
+
+                <div class="flex flex-row items-center justify-between mt-4 py-6">
+                    <button
+                        class="
+                            bg-indigo-600 px-4 py-2 rounded shadow text-white
+                            focus:outline-none focus:shadow-outline-indigo
+                            hover:bg-indigo-700 hover:shadow-lg
+                            transition-all ease-in-out duration-300
+                        "
+                        type="submit"
+                    >
+                        Register
+                    </button>
+
+                    <a
+                        class="
+                            text-gray-900
+                            hover:text-indigo-600
+                            transition-all ease-in-out duration-300
+                        "
+                        href="/login"
+                    >
+                        Already registered?
+                    </a>
+                </div>
+            </form>
+        </div>
+    </div>
+</template>
+
+<script>
+    import InputGroup from "../../components/core/forms/InputGroup";
+
+    export default {
+        name: "Login",
+        components: {InputGroup},
+        layout: 'auth-layout',
+        data() {
+            return {
+                form: {
+                    email: '',
+                    first_name: '',
+                    last_name: '',
+                    password: '',
+                    password_confirmation: '',
+                }
+            }
+        },
+        methods: {
+            submit() {
+                console.log('hey');
+                this.$inertia.post(
+                    this.$route('register'),
+                    this.form
+                );
+            }
+        }
+    }
+</script>
