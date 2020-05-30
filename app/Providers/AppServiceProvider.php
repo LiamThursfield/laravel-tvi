67aa26ee0116b8c7d6cfbd91da0e0ff9c58163c6
Liam Thursfield <liam.thursfield@nexuspoint.co.uk>
2020-05-30 14:26:00 +0100

    Create inertia service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,44 +27,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->inertiaSetup();
-    }
-
-    protected function inertiaSetup()
-    {
-        // Set the root view based on whether the route is for app or admin
-        if(request()->is('admin/*')){
-            Inertia::setRootView('admin');
-        } elseif (request()->is(['login', 'register', 'password/*'])) {
-            Inertia::setRootView('auth');
-        } else {
-            Inertia::setRootView('app');
-        }
-
-        // Enable asset versioning
-        Inertia::version(function () {
-            return md5_file(public_path('mix-manifest.json'));
-        });
-
-        // Shared data
-        Inertia::share([
-            'app' => [
-                'name' => Config::get('app.name')
-            ],
-            'auth' => function () {
-                return [
-                    'user' => Auth::user() ? [
-                        'email' => Auth::user()->email,
-                        'id'    => Auth::user()->id,
-                        'name'  => Auth::user()->name
-                    ] : null
-                ];
-            },
-            'errors' => function () {
-                return Session::get('errors')
-                    ? Session::get('errors')->getBag('default')->getMessages()
-                    : (object) [];
-            },
-        ]);
+        //
     }
 }
---
+++ app/Providers/InertiaServiceProvider.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Session;
+use Inertia\Inertia;
+
+class InertiaServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap inertia
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->setView();
+        $this->versionAssets();
+        $this->shareData();
+    }
+
+
+    /**
+     * Set the root view based on whether the route is for app, auth or admin
+     */
+    protected function setView()
+    {
+        if(request()->is('admin/*')){
+            Inertia::setRootView('admin');
+        } elseif (request()->is(['login', 'register', 'password/*'])) {
+            Inertia::setRootView('auth');
+        } else {
+            Inertia::setRootView('app');
+        }
+    }
+
+
+    /**
+     * Share base data with inertia, including:
+     *  - App data
+     *  - Auth data
+     *  - Errors
+     */
+    protected function shareData()
+    {
+        Inertia::share([
+            'app' => [
+                'name' => Config::get('app.name')
+            ],
+            'auth' => function () {
+                return [
+                    'user' => Auth::user() ? [
+                        'email' => Auth::user()->email,
+                        'id'    => Auth::user()->id,
+                        'name'  => Auth::user()->name
+                    ] : null
+                ];
+            },
+            'errors' => function () {
+                return Session::get('errors')
+                    ? Session::get('errors')->getBag('default')->getMessages()
+                    : (object) [];
+            },
+        ]);
+    }
+
+
+    /**
+     * Enable asset versioning
+     */
+    protected function versionAssets()
+    {
+        Inertia::version(function () {
+            return md5_file(public_path('mix-manifest.json'));
+        });
+    }
+}
