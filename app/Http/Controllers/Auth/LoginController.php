80dc6d7e47ea6a4fe9dc830a2df2b79fa0d060b2
Liam Thursfield <liam3006x@gmail.com>
2020-05-30 15:11:56 +0100

    Remove redundant inpirt

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
-use Illuminate\Support\Facades\Request;
 use Inertia\Inertia;
 use Inertia\Response;
 
