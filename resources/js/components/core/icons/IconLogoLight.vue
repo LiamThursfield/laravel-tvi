2cd1a041853bffc86aa25c7a8df568b82145b916
Liam Thursfield <liam.thursfield@nexuspoint.co.uk>
2020-10-12 18:00:46 +0100

    Add icon

---
+++ resources/js/components/core/icons/IconLogoLight.vue
@@ -0,0 +1,28 @@
+<template>
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 193 179"
+    >
+        <path
+            d="M127 19H20M100.876 159.42l-81-140.296M132.124 106.124l-31 53.694"
+            stroke="#1F243A"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="28"
+        />
+        <path
+            d="M174 20h-1"
+            stroke="#1C64F1"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="28"
+        />
+    </svg>
+</template>
+
+<script>
+    export default {
+        name: "IconLogoLight"
+    }
+</script>
