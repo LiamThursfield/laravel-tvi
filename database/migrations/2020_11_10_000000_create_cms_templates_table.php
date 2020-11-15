3add365843a6897af1289ae25baf2a2740a8a8ec
LiamThursfield <liam3006x@gmail.com>
2020-11-15 22:22:22 +0000

    Initial CMS Template & Template Field Migrations

---
+++ database/migrations/2020_11_10_000000_create_cms_templates_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use App\Interfaces\CMS\TemplateInterface;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCmsTemplatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cms_templates', function (Blueprint $table) {
+            // Field Creation
+            $table->id();
+            $table->string('type', TemplateInterface::FIELD_TYPE_MAX_LENGTH);
+            $table->string('name', TemplateInterface::FIELD_NAME_MAX_LENGTH);
+            $table->text('description')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+
+            // Index Creation
+            $table->index('type');
+            $table->index('name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cms_templates');
+    }
+}
