e8850578116c79410a2de1db6bd7a04fb95ebbb2
LiamThursfield <liam3006x@gmail.com>
2020-11-17 22:35:35 +0000

    Implement Template Field factory

--- app/Models/CMS/TemplateField.php
+++ app/Models/CMS/TemplateField.php
@@ -2,10 +2,13 @@
 
 namespace App\Models\CMS;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class TemplateField extends Model
 {
+    use HasFactory;
+
     protected $table = 'cms_template_fields';
 
     protected $guarded = [];
