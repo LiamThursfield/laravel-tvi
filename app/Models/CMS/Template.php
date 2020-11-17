3a14b7705a743183eb494ae441a8c7423940bca4
LiamThursfield <liam3006x@gmail.com>
2020-11-17 22:27:59 +0000

    Template - Add factory trait

--- app/Models/CMS/Template.php
+++ app/Models/CMS/Template.php
@@ -2,10 +2,13 @@
 
 namespace App\Models\CMS;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Template extends Model
 {
+    use HasFactory;
+
     protected $table = 'cms_templates';
 
     protected $guarded = [];
