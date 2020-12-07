fc5e930d71b309c50d8e68402aec5ba569071332
LiamThursfield <liam3006x@gmail.com>
2020-12-07 16:48:05 +0000

    Add api endpoint to get a teplate

--- routes/admin-api.php
+++ routes/admin-api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminApi\CMS\TemplateController;
 use App\Http\Controllers\AdminApi\FileManager\FileManagerDirectoryController;
 use App\Http\Controllers\AdminApi\FileManager\FileManagerFileController;
 use Illuminate\Support\Facades\Route;
@@ -9,6 +10,13 @@ use Illuminate\Support\Facades\Route;
 | Admin API Routes
 |--------------------------------------------------------------------------
 */
+
+Route::group([
+    'as' => 'cms.',
+    'prefix' => 'cms'
+], function() {
+    Route::get('/template/{template}', [TemplateController::class, 'show'])->name('templates.index');
+});
 
 Route::group([
     'as' => 'file-manager.',
