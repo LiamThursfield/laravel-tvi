697bd29174446fe2d39fcaf383453bcedacecb13
LiamThursfield <liam3006x@gmail.com>
2020-12-13 16:36:42 +0000

    Create CMS Content Resource

--- app/Http/Resources/Admin/CMS/LayoutResource.php
+++ app/Http/Resources/Admin/CMS/LayoutResource.php
@@ -9,12 +9,20 @@ class LayoutResource extends JsonResource
 
     public function toArray($request)
     {
-        return [
+        $data = [
             'id'            => $this->id,
             'name'          => $this->name,
             'slug'          => $this->slug,
             'template'      => TemplateResource::make($this->whenLoaded('template')),
             'template_id'   => $this->template_id,
         ];
+
+        // Add content, keyed via template field id
+        if ($this->resource->relationLoaded('content')) {
+            $content = ContentResource::collection($this->content);
+            $data['content'] = $content->keyBy('template_field_id');
+        }
+
+        return $data;
     }
 }
