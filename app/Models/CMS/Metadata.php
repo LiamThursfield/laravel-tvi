585db9e5f6db4bd317240243d44ca8bb2cea9983
LiamThursfield <liam3006x@gmail.com>
2021-02-06 18:05:28 +0000

    Metadata - add MorphTo relationship

--- app/Models/CMS/Metadata.php
+++ app/Models/CMS/Metadata.php
@@ -4,6 +4,7 @@ namespace App\Models\CMS;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
 
 class Metadata extends Model
 {
@@ -12,4 +13,9 @@ class Metadata extends Model
     protected $table = 'cms_metadata';
 
     protected $guarded = [];
+
+    public function metadatable() : MorphTo
+    {
+        return $this->morphTo();
+    }
 }
