7579025c650bf998b6ca1180d692b20a63c3db9f
LiamThursfield <liam3006x@gmail.com>
2021-02-19 00:35:06 +0000

    Authorise FileManager apis with permissions

--- app/Http/Controllers/AdminApi/FileManager/AbstractFileManagerController.php
+++ app/Http/Controllers/AdminApi/FileManager/AbstractFileManagerController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\AdminApi\FileManager;
 
 use App\Http\Controllers\Controller;
+use App\Interfaces\PermissionInterface;
 
 class AbstractFileManagerController extends Controller
 {
@@ -14,6 +15,14 @@ class AbstractFileManagerController extends Controller
     {
         // TODO: Allow this to be defined via user.
         $this->storage_disk = 'file_manager';
+
+        $this->middleware(
+            PermissionInterface::getMiddlewareString(PermissionInterface::VIEW_FILE_MANAGER)
+        )->only('index');
+
+        $this->middleware(
+            PermissionInterface::getMiddlewareString(PermissionInterface::EDIT_FILE_MANAGER)
+        )->only('store');
     }
 
 }
