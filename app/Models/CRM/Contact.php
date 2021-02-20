cad76a4fbff4229da5505f6c09421ef0b4e43201
LiamThursfield <liam3006x@gmail.com>
2021-02-20 13:39:29 +0000

    Implement CRM Contact model/migration

---
+++ app/Models/CRM/Contact.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models\CRM;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Contact extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $table = 'crm_contact';
+
+    protected $guarded = [];
+
+    protected $appends = [
+        'name'
+    ];
+
+
+    /**
+     * Helper to return first + last name.
+     * @return string
+     */
+    public function getNameAttribute() : String
+    {
+        return trim($this->first_name ?? '' . ' ' . $this->last_name ?? '');
+    }
+}
---
+++ database/migrations/2021_02_18_000000_create_crm_contacts_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCrmContactsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('crm_contacts', function (Blueprint $table) {
+            // Field Creation
+            $table->id();
+            $table->string('title')->nullable();
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
+            $table->string('telephone')->nullable();
+            $table->string('email')->nullable();
+            $table->boolean('marketing_telephone')->default(false);
+            $table->boolean('marketing_sms')->default(false);
+            $table->boolean('marketing_email')->default(false);
+            $table->timestamps();
+            $table->softDeletes();
+
+            // Index Creation
+            $table->index('first_name');
+            $table->index('last_name');
+            $table->index('telephone');
+            $table->unique('email');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('crm_contacts');
+    }
+}
