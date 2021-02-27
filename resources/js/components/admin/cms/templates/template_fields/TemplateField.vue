65d7a6a71fd9f9bb634a262b40819a26b55115f4
LiamThursfield <liam3006x@gmail.com>
2021-02-27 13:26:44 +0000

    Whitespace fix

--- resources/js/components/admin/cms/templates/template_fields/TemplateField.vue
+++ resources/js/components/admin/cms/templates/template_fields/TemplateField.vue
@@ -107,7 +107,7 @@
     import CheckboxGroup    from "../../../../core/forms/CheckboxGroup";
     import InputGroup       from "../../../../core/forms/InputGroup";
     import NumberSettings   from "./template_field_settings/NumberSettings";
-    import RepeaterSettings   from "./template_field_settings/RepeaterSettings";
+    import RepeaterSettings from "./template_field_settings/RepeaterSettings";
     import SelectGroup      from "../../../../core/forms/SelectGroup";
     import TextSettings     from "./template_field_settings/TextSettings";
     import TextAreaSettings from "./template_field_settings/TextAreaSettings";
