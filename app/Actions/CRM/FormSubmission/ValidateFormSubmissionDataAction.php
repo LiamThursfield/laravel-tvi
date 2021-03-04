1105250613708834b729275cc58f457acb5b0f56
LiamThursfield <liam3006x@gmail.com>
2021-03-04 23:51:55 +0000

    Implement action to validate Form Submission data

---
+++ app/Actions/CRM/FormSubmission/ValidateFormSubmissionDataAction.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Actions\CRM\FormSubmission;
+
+use App\Interfaces\CRM\FormFieldInterface;
+use App\Models\CRM\Form;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
+
+class ValidateFormSubmissionDataAction
+{
+    /**
+     * @param Form $form
+     * @param array $submissionData
+     * @return array
+     * @throws ValidationException
+     */
+    public function handle(Form $form, array $submissionData) : array
+    {
+        if (!$form->relationLoaded('formFields')) {
+            $form->load('formFields');
+        }
+
+
+        $rules = [];
+        $messages = [];
+
+        foreach ($form->formFields as $field) {
+            $fieldRules = [];
+
+            if ($field->is_required) {
+                $fieldRules[] = 'required';
+                $messages[$field->slug . '.required'] = 'The ' . $field->name . ' field is required.';
+            } else {
+                $fieldRules[] = 'nullable';
+            }
+
+            if (in_array($field->type, [FormFieldInterface::TYPE_EMAIL, FormFieldInterface::TYPE_CRM_EMAIL])) {
+                $fieldRules[] = 'email';
+                $messages[$field->slug . '.email'] = 'The ' . $field->name . ' field must be a valid email address.';
+            } elseif ($field->type === FormFieldInterface::TYPE_NUMBER) {
+                $fieldRules[] = 'numeric';
+                $messages[$field->slug . '.numeric'] = 'The ' . $field->name . ' field must be a number.';
+            }
+
+            $rules[$field->slug] = implode('|', $fieldRules);
+        }
+
+        $validator = Validator::make($submissionData, $rules, $messages);
+
+        return $validator->validate();
+    }
+
+}
