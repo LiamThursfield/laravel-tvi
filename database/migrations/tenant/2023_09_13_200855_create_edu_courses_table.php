fc5e16581bbe53aa92b00ab73f4f4c6a6f947f2c
liamthursfield <liam3006x@gmail.com>
2023-10-03 02:00:48 +0100

    WIP Checkout Webhook

--- database/migrations/tenant/2023_09_13_200855_create_edu_courses_table.php
+++ database/migrations/tenant/2023_09_13_200855_create_edu_courses_table.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Interfaces\EDU\Course\CourseInterface;
+use App\Interfaces\EDU\Purchase\PurchaseInterface;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -32,7 +33,7 @@ return new class extends Migration
             // Price
             $table->string('price')->nullable();
             $table->string('discount_price')->nullable();
-            $table->string('currency')->nullable()->default(CourseInterface::CURRENCY_GB);
+            $table->string('currency')->nullable()->default(PurchaseInterface::CURRENCY_GB);
 
             // Webinars
             $table->boolean('has_webinars')->nullable()->default(false);
