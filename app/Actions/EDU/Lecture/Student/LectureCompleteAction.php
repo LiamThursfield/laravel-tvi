e5e5c702e57d441e821b251ed822b8f22be4008a
liamthursfield <liam3006x@gmail.com>
2023-10-24 22:14:59 +0100

    Mark purchases as non-refundable

--- app/Actions/EDU/Lecture/Student/LectureCompleteAction.php
+++ app/Actions/EDU/Lecture/Student/LectureCompleteAction.php
@@ -2,13 +2,37 @@
 
 namespace App\Actions\EDU\Lecture\Student;
 
+use App\Models\EDU\Course\Course;
 use App\Models\EDU\Lecture\Lecture;
+use App\Models\EDU\Purchase\Purchase;
+use App\Models\EDU\Purchase\PurchaseItem;
+use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 
 class LectureCompleteAction
 {
     public function handle(Lecture $lecture): void
     {
-        $lecture->users()->attach(Auth::id(), ['completed' => true]);
+        $user = Auth::user();
+
+        $this->markCoursesAsNonRefundable($user, $lecture);
+        $lecture->users()->attach($user->id, ['completed' => true]);
+    }
+
+
+    protected function markCoursesAsNonRefundable(User $user, Lecture $lecture)
+    {
+        $purchaseItems = PurchaseItem::where('purchasable_id', $lecture->section->course_id)
+            ->where('purchasable_type', Course::class)
+            ->pluck('purchase_id');
+
+        $purchases = Purchase::where('user_id', $user->id)
+            ->whereIn('id', $purchaseItems)
+            ->get();
+
+        foreach ($purchases as $purchase) {
+            $purchase->is_refundable = false;
+            $purchase->save();
+        }
     }
 }
