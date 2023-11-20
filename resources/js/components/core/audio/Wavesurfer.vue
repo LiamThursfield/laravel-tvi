605839a704acfb8fe4469f19848e97bac79c7d60
Liam Thursfield <liam3006x@gmail.com>
2023-11-20 22:46:53 +0000

    Further wavesurfer updates

--- resources/js/components/core/audio/Wavesurfer.vue
+++ resources/js/components/core/audio/Wavesurfer.vue
@@ -10,15 +10,39 @@
         props: {
             audioUrl: {
                 type: String,
-                required: true,
+                required: false,
             },
             containerIdOverride: {
                 type: String,
                 default: null,
             },
+            cursorColor: {
+                type: String,
+                default: "#111"
+            },
+            cursorWidth: {
+                type: Number,
+                default: 0
+            },
+            barGap: {
+                type: [Number, NaN],
+                default: 0
+            },
+            barRadius: {
+                type: [Number, NaN],
+                default: 50
+            },
+            barWidth: {
+                type: [Number, NaN],
+                default: 3
+            },
             height: {
                 type: [String, Number],
                 default: "8em"
+            },
+            media: {
+                type: String,
+                required: false,
             },
             mediaControls: {
                 type: Boolean,
@@ -27,6 +51,22 @@
             normalize: {
                 type: Boolean,
                 default: false
+            },
+            progressColor: {
+                type: String,
+                default: "#555"
+            },
+            splitChannels: {
+                type: Boolean,
+                default: false,
+            },
+            theme: {
+                type: String,
+                default: "base"
+            },
+            waveColor: {
+                type: String,
+                default: "#999"
             },
             width: {
                 type: [String, Number],
@@ -41,14 +81,30 @@
         },
         computed: {
             wavesurferOptions() {
-                return {
+                let options = {
+                    barGap: this.barGap,
+                    barRadius: this.barRadius,
+                    barWidth: this.barWidth,
                     container: "#" + this.containerId,
+                    cursorWidth: this.cursorWidth,
                     height: this.height,
                     mediaControls: this.mediaControls,
                     normalize: this.normalize,
-                    url: this.audioUrl,
+                    splitChannels: this.splitChannels,
                     width: this.width,
                 }
+
+                if (this.media) {
+                    options.media = this.media;
+                } else if (this.audioUrl) {
+                    options.url = this.audioUrl;
+                } else {
+                    console.error('No valid url or media source set');
+                }
+
+                options = Object.assign(options, this.getColorOptions());
+
+                return options;
             }
         },
         created() {
@@ -62,6 +118,31 @@
             this.$nextTick(() => {
                 this.wavesurfer = WaveSurfer.create(this.wavesurferOptions);
             })
+        },
+        methods: {
+            getColorOptions() {
+                let defaultColors = {
+                    cursorColor: this.cursorColor,
+                    progressColor: this.progressColor,
+                    waveColor: this.waveColor,
+                };
+
+                if (!this.theme) {
+                    return defaultColors;
+                }
+
+                switch (this.theme) {
+                    case 'primary':
+                        return {
+                            cursorColor: "#1D4ED8",
+                            progressColor: "#2563EB",
+                            waveColor: "#666666",
+                        }
+                    case 'base':
+                    default:
+                        return defaultColors;
+                }
+            }
         }
     }
 </script>
