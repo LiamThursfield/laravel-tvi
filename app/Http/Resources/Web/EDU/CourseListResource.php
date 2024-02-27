677d21d83695211521c49a5b4a66b91f87f7495d
Liam Thursfield <liam3006x@gmail.com>
2024-02-27 23:20:34 +0000

    feat: Implement BE that allows for CMS pages to be created with course(s) attached

--- app/Http/Resources/Web/EDU/CourseListResource.php
+++ app/Http/Resources/Web/EDU/CourseListResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources\Web\EDU;
 
+use App\Interfaces\EDU\Course\CourseInterface;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -11,10 +12,12 @@ class CourseListResource extends JsonResource
     {
         return [
             'discount_price' => $this->discount_price,
+            'id' => $this->id,
             'name' => $this->name,
             'price' => $this->price,
             'primary_image' => $this->primary_image,
             'slug' => $this->slug,
+            'status' => CourseInterface::STATUSES[$this->status]
         ];
     }
 }
--- routes/admin-api.php
+++ routes/admin-api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\AdminApi\CMS\TemplateController;
 use App\Http\Controllers\AdminApi\CMS\UrlAvailableController;
 use App\Http\Controllers\AdminApi\CRM\FormController;
 use App\Http\Controllers\AdminApi\CRM\OrganisationUnitController;
+use App\Http\Controllers\AdminApi\EDU\CourseController;
 use App\Http\Controllers\AdminApi\FileManager\FileManagerDirectoryController;
 use App\Http\Controllers\AdminApi\FileManager\FileManagerFileController;
 use Illuminate\Support\Facades\Route;
@@ -34,6 +35,13 @@ Route::group([
     Route::get('/organisation-units', [OrganisationUnitController::class, 'index'])->name('organisation-units.index');
 
     Route::get('/form', [FormController::class, 'index'])->name('forms.index');
+});
+
+Route::group([
+    'as' => 'edu.',
+    'prefix' => 'edu'
+], function() {
+    Route::get('/course', [CourseController::class, 'index'])->name('courses.index');
 });
 
 Route::group([
