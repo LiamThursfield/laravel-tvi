78dbbc6d748f3704ad98ab2bdf580f999e63f9f2
dorusomcutean <somcuteandoru@hotmail.com>
2024-03-09 19:49:18 +0000

    remove unused import

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,7 +7,6 @@
         <title>SIGI</title>
 
         <link href="{{ mix('/css/app.css') }}" rel="stylesheet"/>
-        <link href="https://cdn.jsdelivr.net/npm/keen-slider@6.8.6/keen-slider.min.css" rel="stylesheet" />
 
 
         @routes
