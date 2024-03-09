87187ec98db900f1b0b9059007f56f6505ca3c18
dorusomcutean <somcuteandoru@hotmail.com>
2024-03-09 22:07:18 +0000

    WIP

--- app/Actions/EDU/Course/Student/StudentCourseShowAction.php
+++ app/Actions/EDU/Course/Student/StudentCourseShowAction.php
@@ -15,6 +15,7 @@ class StudentCourseShowAction
         $course->load([
             'sections',
             'sections.webinars',
+            'sections.files',
             'webinars',
             'creator',
             'labels',
@@ -32,6 +33,7 @@ class StudentCourseShowAction
                 }
             }
         }
+//        dd($course->toArray());
 
         CourseResource::withoutWrapping();
         return CourseResource::make($course);
--- app/Http/Resources/Admin/EDU/Section/SectionResource.php
+++ app/Http/Resources/Admin/EDU/Section/SectionResource.php
@@ -5,6 +5,8 @@ namespace App\Http\Resources\Admin\EDU\Section;
 use App\Http\Resources\Admin\EDU\Lecture\LectureResource;
 use App\Http\Resources\Admin\EDU\Webinar\WebinarResource;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Storage;
 
 class SectionResource extends JsonResource
 {
@@ -22,6 +24,27 @@ class SectionResource extends JsonResource
             'course_id' => $this->course_id,
             'child_items' => LectureResource::collection($this->whenLoaded('childItems')),
             'webinar' => WebinarResource::make($this->whenLoaded('webinar')),
+            'files' => $this->getFiles() ?? null,
         ];
+    }
+
+    protected function getFiles()
+    {
+        if (!$this->files) {
+            return null;
+        }
+
+        $files = $this->files;
+
+        foreach ($files as $file) {
+            $file->url = Storage::disk('file_manager')->temporaryUrl(
+                $file->file_path, Carbon::now()->addHour()
+            );
+            if (str_contains($file->file_name, '.mp3')) {
+                $file->type_audio = true;
+            }
+        }
+
+        return $files;
     }
 }
