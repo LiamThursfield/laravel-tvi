8e7b1c468d875352235618224b681da011550db0
Liam Thursfield <liam3006x@gmail.com>
2024-03-13 23:00:49 +0000

    Commit

--- app/Models/Tenant.php
+++ app/Models/Tenant.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Models\Settings\CoreSettings;
+use Exception;
 use Stancl\Tenancy\Database\Models\Tenant as BaseTenant;
 use Stancl\Tenancy\Contracts\TenantWithDatabase;
 use Stancl\Tenancy\Database\Concerns\HasDatabase;
@@ -14,6 +15,11 @@ class Tenant extends BaseTenant implements TenantWithDatabase
 
     public function getLocaleAttribute(): string
     {
-        return app()->make(CoreSettings::class)->locale;
+        try {
+            return app()->make(CoreSettings::class)->locale;
+        } catch (Exception $e) {
+            // For when a tenant is being created
+            return 'en';
+        }
     }
 }
