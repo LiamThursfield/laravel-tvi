2c1e809ea2107f13531b6a1053a11b31ab55773f
Liam Thursfield <liam3006x@gmail.com>
2024-04-28 19:14:26 +0100

    Initial fix for tests
    - Tests still take too long to run per test but can at least test!

---
+++ database/seeders/EmptySeeder.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class EmptySeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run() {}
+}
